abf059a879f7a390fb5ebe197e8aa2d68ebcd928
grupo30 <pi.grupo30@gmail.com>
2022-11-18 16:34:00 -0300

    primeiro-ro

---
+++ Model/Member.php
@@ -0,0 +1,154 @@
+<?php
+namespace Phppot;
+
+class Member
+{
+
+    private $ds;
+
+    function __construct()
+    {
+        require_once __DIR__ . '/../lib/DataSource.php';
+        $this->ds = new DataSource();
+    }
+
+    /**
+     * verificar se um usuario existe
+     *
+     * @param string $username
+     * @return boolean
+     */
+    public function isUsernameExists($username)
+    {
+        $query = 'SELECT * FROM tbl_member where username = ?';
+        $paramType = 's';
+        $paramValue = array(
+            $username
+        );
+        $resultArray = $this->ds->select($query, $paramType, $paramValue);
+        $count = 0;
+        if (is_array($resultArray)) {
+            $count = count($resultArray);
+        }
+        if ($count > 0) {
+            $result = true;
+        } else {
+            $result = false;
+        }
+        return $result;
+    }
+
+    /**
+     * verificar se um email existe
+     *
+     * @param string $email
+     * @return boolean
+     */
+    public function isEmailExists($email)
+    {
+        $query = 'SELECT * FROM tbl_member where email = ?';
+        $paramType = 's';
+        $paramValue = array(
+            $email
+        );
+        $resultArray = $this->ds->select($query, $paramType, $paramValue);
+        $count = 0;
+        if (is_array($resultArray)) {
+            $count = count($resultArray);
+        }
+        if ($count > 0) {
+            $result = true;
+        } else {
+            $result = false;
+        }
+        return $result;
+    }
+
+    /**
+     * entrar / registrar
+     *
+     * @return string[] registrar status msg
+     */
+    public function registerMember()
+    {
+        $isUsernameExists = $this->isUsernameExists($_POST["username"]);
+        $isEmailExists = $this->isEmailExists($_POST["email"]);
+        if ($isUsernameExists) {
+            $response = array(
+                "status" => "error",
+                "message" => "Usuario já existe"
+            );
+        } else if ($isEmailExists) {
+            $response = array(
+                "status" => "error",
+                "message" => "E-mail já existe"
+            );
+        } else {
+            if (! empty($_POST["signup-password"])) {
+
+                
+                $hashedPassword = password_hash($_POST["signup-password"], PASSWORD_DEFAULT); //funcao de cryptografia 
+            }
+            $query = 'INSERT INTO tbl_member (username, password, email) VALUES (?, ?, ?)';
+            $paramType = 'sss';
+            $paramValue = array(
+                $_POST["username"],
+                $hashedPassword,
+                $_POST["email"]
+            );
+            $memberId = $this->ds->insert($query, $paramType, $paramValue);
+            if (! empty($memberId)) {
+                $response = array(
+                    "status" => "Sucesso!",
+                    "message" => "Voce se registrou com sucesso!"
+                );
+            }
+        }
+        return $response;
+    }
+
+    public function getMember($username)
+    {
+        $query = 'SELECT * FROM tbl_member where username = ?';
+        $paramType = 's';
+        $paramValue = array(
+            $username
+        );
+        $memberRecord = $this->ds->select($query, $paramType, $paramValue);
+        return $memberRecord;
+    }
+
+    /**
+     * para entrar usuario*
+     *
+     * @return string
+     */
+    public function loginMember()
+    {
+        $memberRecord = $this->getMember($_POST["username"]);
+        $loginPassword = 0;
+        if (! empty($memberRecord)) {
+            if (! empty($_POST["login-password"])) {
+                $password = $_POST["login-password"];
+            }
+            $hashedPassword = $memberRecord[0]["password"];
+            $loginPassword = 0;
+            if (password_verify($password, $hashedPassword)) {
+                $loginPassword = 1;
+            }
+        } else {
+            $loginPassword = 0;
+        }
+        if ($loginPassword == 1) {
+           
+            session_start();
+            $_SESSION["username"] = $memberRecord[0]["username"];
+            session_write_close();
+            $url = "./home.php";
+            header("Location: $url");
+        } else if ($loginPassword == 0) {
+            $loginStatus = "Loguin ou senha inválidos.";
+            return $loginStatus;
+        }
+    }
+}
